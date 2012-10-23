d6a0eb286009ed4e26241830b0df2a40e498c448
Jochen Keil <jochen.keil@gmail.com>
2012-10-23 12:29:53 +0200

    Replace initialization code with call to makeGroup

--- group.c
+++ group.c
@@ -3,18 +3,11 @@
 #include <string.h>
 
 void initGroups ( GroupData * gd ) {
-    int i, k;
+    int i;
 
     memset ( gd->group, 0, gd->ngroups * sizeof ( group_t ) );
 
     for ( i = 0; i < gd->ngroups; i++ ) {
-        gd->group[i].cmp_type = gd->comp[i];
-        makeGroup ( &(gd->group[i]), gd->size[i] );
-
-        for ( k = 0; k < gd->size[i]; k++ ) {
-            gd->group[i].cluster[k].mean = gd->init ( k, gd->size[i] );
-        }
-
-        seedGroup ( &(gd->group[i]), gd->seed[i] );
+        makeGroup ( gd->init, &(gd->group[i]), gd->size[i], gd->comp[i], gd->seed[i] );
     }
 }
