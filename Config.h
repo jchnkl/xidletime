4abe8b06197d00f4ea76f1ef568550fe10c3aec3
Jochen Keil <jochen.keil@gmail.com>
2012-10-23 20:59:42 +0200

    Compile time options header file

---
+++ Config.h
@@ -0,0 +1,8 @@
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+const char * busName       = "org.xidletime";
+const char * objectPath    = "/org/xidletime";
+const char * interfaceName = "org.xidletime";
+
+#endif
