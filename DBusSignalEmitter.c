b907fa2ab7e02f942b9488d75b047f6a57ab51e0
Jochen Keil <jochen.keil@gmail.com>
2012-10-23 10:45:29 +0200

    Remove goto; replace with return

--- DBusSignalEmitter.c
+++ DBusSignalEmitter.c
@@ -6,20 +6,20 @@ int initDBus ( DBusConfig * dbusconfig ) {
 
     DBusError        err;
     DBusConnection * conn;
-    int              ret = 0;
 
     dbus_error_init ( &err ); // initialise the errors
 
-    if ( dbusconfig == NULL ) { ret = -1; goto exit; }
+    if ( dbusconfig == NULL ) return -1;
+
     if ( dbusconfig->busName == NULL
       || dbusconfig->objectPath == NULL
       || dbusconfig->interfaceName == NULL
-       ) { ret = -1; goto exit; }
+       ) return -1;
 
     // connect to the bus
     conn = dbus_bus_get ( DBUS_BUS_SESSION, &err );
 
-    if ( dbus_error_is_set ( &err ) || conn == NULL ) { ret = -1; goto exit; }
+    if ( dbus_error_is_set ( &err ) || conn == NULL ) return -1;
 
     // request a name on the bus
     if ( dbus_bus_request_name ( conn
@@ -29,13 +29,11 @@ int initDBus ( DBusConfig * dbusconfig ) {
                                )
          != DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER
       || dbus_error_is_set ( &err )
-       ) { ret = -1; goto exit; }
+       ) return -1;
 
     dbusconfig->connection = conn;
 
-exit:
-    dbus_error_free ( &err );
-    return ret;
+    return 0;
 
 }
 
@@ -57,20 +55,19 @@ int dbusEmitSignal ( DBusConfig * dbusconfig ) {
                                   , dbusconfig->signalName
                                   );
 
-    if ( msg == NULL ) { ret = -1; goto exit; }
+    if ( msg == NULL ) return -1;
 
     // send the message and flush the connection
     if ( ! dbus_connection_send ( dbusconfig->connection
                                 , msg
                                 , &serial
                                 )
-       ) { ret = -1; goto exit; }
+       ) return -1;
 
     dbus_connection_flush ( dbusconfig->connection );
 
-exit:
     dbus_message_unref ( msg );
-    return ret;
+    return 0;
 
 }
 
