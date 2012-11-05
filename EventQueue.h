81564b69b34d89a77707985dda23709062636154
Jochen Keil <jochen.keil@gmail.com>
2012-11-05 21:07:46 +0100

    New data types Event{Source,Sink} and lookup table

--- EventQueue.h
+++ EventQueue.h
@@ -20,6 +20,32 @@ typedef struct EventQueueT
     ;
     } EventQueueT;
 
+typedef struct EventSourceT
+    { IdentT id;
+    ; EventQueueT * eq
+    ; PublicConfig * public
+    ; PrivateConfig * private
+    ; eventRunner er
+    ; eventCallback ec
+    ;
+    } EventSourceT;
+
+typedef struct EventSinkT
+    { IdentT id
+    ; PublicConfig * public
+    ; PrivateConfig * private
+    ; void ( * callback ) ( struct EventSinkT *, EventSourceT * )
+    ;
+    } EventSinkT;
+
+typedef struct SourceSinkTableT
+    { unsigned int   numSources;
+    ; EventSourceT * sources;
+    ; unsigned int   numSinks;
+    ; EventSinkT   * sinks;
+    ;
+    } SourceSinkTableT;
+
 EventQueueT * makeEventQueue ( EventQueueT * );
 
 void destroyEventQueue ( EventQueueT * );
