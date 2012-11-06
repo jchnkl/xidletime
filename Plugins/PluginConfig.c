e897eee9c903d10137cc44032109440bfb3008e0
Jochen Keil <jochen.keil@gmail.com>
2012-11-06 20:21:38 +0100

    Add plugins to static config table
    Using some ids which don't start with 0, to test things a bit.

--- Plugins/PluginConfig.c
+++ Plugins/PluginConfig.c
@@ -1,18 +1,23 @@
 #include "PluginConfig.h"
 
 #include "HelloWorld.h"
+#include "XIdleTimer.h"
+#include "AdaptiveTimeout.h"
+#include "DBusInterface.h"
 
 const EventSourceConfigT sourceConfig[] =
-    { {  0, NULL, helloWorldSource, NULL }
+    { {  1, NULL, xIdleTimerSource, NULL }
     , { -1, NULL, NULL,             NULL }
     };
 
 const EventSinkConfigT sinkConfig[] =
-    { {  0, NULL, helloWorldSink }
-    , { -1, NULL, NULL           }
+    { {  1, NULL, xIdleTimerSink      }
+    , {  2, NULL, adaptiveTimeoutSink }
+    , {  3, NULL, dbusSendSignalSink  }
+    , { -1, NULL, NULL                }
     };
 
 const WireTableConfigT wireTable[] =
-    { {  1, 0, { 0 } }
-    , { -1, 0, {}    }
+    { {  3, 1, { 1, 2, 3 } }
+    , { -1, 0, {      } }
     };
