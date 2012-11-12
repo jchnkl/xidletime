43f93e1a583ae82aeb3a6aa79fd1989d5ee687fd
Jochen Keil <jochen.keil@gmail.com>
2012-11-12 11:59:43 +0100

    Update static configuration of plugins

--- Plugins/PluginConfig.c
+++ Plugins/PluginConfig.c
@@ -7,18 +7,22 @@
 #include "XBacklight.h"
 
 const EventSourceConfigT sourceConfig[] =
-    { {  1, NULL, xIdleTimerSource, NULL }
-    , { -1, NULL, NULL,             NULL }
+    { {  1, NULL, xIdleTimerSource,  NULL }
+    , {  2, NULL, dbusReceiveSource, NULL }
+    , { -1, NULL, NULL,              NULL }
     };
 
 const EventSinkConfigT sinkConfig[] =
-    { {  1, NULL, xIdleTimerSink      }
-    , {  2, NULL, adaptiveTimeoutSink }
-    , {  3, NULL, dbusSendSignalSink  }
-    , { -1, NULL, NULL                }
+    { {  1, NULL, xIdleTimerSink             }
+    , {  2, NULL, adaptiveTimeoutSink        }
+    , {  3, NULL, dbusEmitSignalSink         }
+    , {  4, NULL, dbusScreenSaverSuspendSink }
+    , {  5, NULL, xBacklightSink             }
+    , { -1, NULL, NULL                       }
     };
 
 const WireTableConfigT wireTable[] =
-    { {  3, 1, { 1, 2, 3 } }
-    , { -1, 0, {      } }
+    { {  3, 1, { 1, 2, 3, 5 } }
+    , {  1, 2, { 4          } }
+    , { -1, 0, {            } }
     };
